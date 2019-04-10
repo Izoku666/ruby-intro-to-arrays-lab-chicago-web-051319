180f322da23ce437a8629076cb36c14232d57966
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-10 01:08:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,10 +8,21 @@ end
 
 def first_element(array)
   return array.first
+  # also can use return array[0]
 end
 
 def third_element(array)
   return array[2]
+end
+
+def last_element(array)
+  #return array.last
+  #also can use 
+  return array[array.length - 1]
+end
+
+def first_element_with_array_methods(array)
+  return array.first
 end
 
 
